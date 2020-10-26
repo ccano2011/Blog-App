0c77519c01e46d61604ee809d9ff07c50d65601f
Annie Wang <anniewang@annies-mbp-2.lan>
2020-10-26 14:04:01 -0400

    set up boilerplate for comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import React from 'react'
+import Home from './screens/Home/Home'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <Route exact path = "/" component = {Home}/>
     </div>
   );
 }
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css"
+import Layout from '../../components/shared/Layout/Layout'
+
+function Home() {
+  
+  return (
+    <Layout>
+    <div className="homepage">
+      
+      </div>
+      </Layout>
+)  
+}
+export default Home;
