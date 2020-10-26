c09e05949ab4660a9e9d24bfb8326313edab7ff2
Brian <briand.nester@gmail.com>
2020-10-26 13:56:00 -0400

    Push to dev branch

---
+++ client/src/components/PostCards/PostCards.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './PostCards.css'
+
+class PostCards extends Component {
+    constructor() {
+        super();
+        this.state = {
+            posts: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default PostCards
---
+++ client/src/components/Search/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Search.css';
+
+const Search = () => {
+  
+    return (
+        <form>
+            <input >/</input>
+        </form>
+    )
+}
+
+export default Search
