88814d281ffac51dcad59417f4122dba02322a05
Annie Wang <anniewang@annies-mbp-2.lan>
2020-10-26 23:52:45 -0400

    populated components

--- client/src/components/PostCards/PostCards.jsx
+++ client/src/components/PostCards/PostCards.jsx
@@ -9,13 +9,26 @@ class PostCards extends Component {
         }
     }
 
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+    async componentDidMount() {
+      const posts = await getPosts()
+      this.setState({ posts })
     }
-}
+  
+    render() {
+  
+      const CARDS = this.state.posts.reverse().map((post, index) =>
+        index < 8 ? <PostCard _id={post._id} content={post.content} title={post.title} imgURL={product.imgURL} key={index} /> : null
+      )
+  
+      return (
+        <div className="post-cards">
+          <div className="latest">LATEST</div>
+          <div className="cards">
+            {CARDS}
+          </div>
+        </div>
+      )
+    }
+  }
 
 export default PostCards
--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -3,11 +3,19 @@ import './Search.css';
 
 const Search = () => {
   
-    return (
-        <form>
-            <input >/</input>
-        </form>
-    )
+  return (
+    <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
+        <input
+            className="search-input"
+            value={props.value}
+            onChange={(e) => props.onChange(e)}
+            name="Search"
+            placeholder="Search"
+            type="text"
+            autoFocus
+        />
+    </form>
+)
 }
 
 export default Search
